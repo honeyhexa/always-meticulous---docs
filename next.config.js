16313cbc23107da2ef667c1871513afb4e8e04a9
Arth K. Gajjar <iarthstar@gmail.com>
2023-11-30 02:25:46 +0530

    added : meticulous docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs',
+        permanent: true,
+      },
+    ]
+  },
 })
 
 module.exports = withNextra()
